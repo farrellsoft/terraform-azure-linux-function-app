e4aeee4dfc252cdf3b4097f9454d8e6ebeea8e09
Jason Farrell <imjason@gmail.com>
2023-01-31 15:55:52 -0500

    fixed property name misspelling

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource azurerm_linux_function_app this {
   site_config {
     always_on               = each.value.always_on
     httpsOnly               = true
-    ftpsState               = "FtpsOnly"
+    ftps_state              = "FtpsOnly"
     application_stack {
       dotnet_version    = each.value.dotnet_version
     }
