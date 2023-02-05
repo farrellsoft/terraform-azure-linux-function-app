4bd2dbf25b0d4a9a41c62da733ee43bbf841f66c
Jason Farrell <imjason@gmail.com>
2023-02-04 19:18:00 -0500

    tweaks and changes

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   app_names_map = {
     for k, v in var.function_apps : k =>
-      "func-${v["application"]}${v["purpose"] == "" ? "" : "-${v["purpose"]}"}-${v["environment"]}-${module.regioncodes.region_codes[lower(replace(v["location"], " ", ""))]}${v["instance"] == "" ? "" : "-${v["instance"]}"}"
+      "func-${v["application"]}-${v["environment"]}-${v["instance"] == "" ? "" : "-${v["instance"]}"}"
   }
 
   system_identity_map = {
