17727b838da0fb1ef591ba1b5a2e371025646e48
Jason Farrell <imjason@gmail.com>
2023-02-19 20:05:23 -0500

    added more outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "name" {
 output id {
   value = azurerm_linux_function_app.this.id
 }
+
+output "default_hostname" {
+  value = azurerm_linux_function_app.this.default_hostname
+}
+
+output "system_assigned_identity_id" {
+  value = azurerm_linux_function_app.this.identity.0.principal_id
+}
